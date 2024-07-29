91b98957cf7f4c7de7bb2ce8cb460a988ca4f9d0
Javier Rosales <cyjav.dev@gmail.com>
2024-07-29 16:54:08 -0600

    Prepare for backend deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const morgan = require("morgan")
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token("request-body", (request, response) => {
@@ -116,7 +118,7 @@ app.delete("/api/people/:id", (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
